3991f7e823e26a0d48f3f3ae3fa8b21858371345
tranduylong95 <tranduylong95@gmail.com>
2023-03-20 02:53:00 +0700

    task 7

---
+++ Practice/Task_7/index.js
@@ -0,0 +1,24 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = 'Hà Nội';
+
+document.write('i = ' + i);
+document.write('<br/>');
+document.write('f = ' + f);
+document.write('<br/>');
+document.write('b = ' + b);
+document.write('<br/>');
+document.write('s = ' + s);
+
+let width = 20;
+let height = 10;
+let area = width * height;
+document.write('Area = ' + area);
+
+// inputWidth = prompt("Enter the width");
+// inputHeight = prompt("Enter the height");
+// let width = parseInt(inputWidth);
+// let height = parseInt(inputHeight);
+// let area = width * height;
+// document.write("The area is: " + area);
