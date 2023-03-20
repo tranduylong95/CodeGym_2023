ab3a03f58eeffcad90f0ee7e024da0b10ca1558e
tranduylong95 <tranduylong95@gmail.com>
2023-03-20 10:17:53 +0700

    task 12

--- Practice/Task_12/index.js
+++ Practice/Task_12/index.js
@@ -8,4 +8,23 @@ while (first < last) {
     first++;
     last--;
 }
-document.write(x);
+ document.write(x);
+ //Tìm giá trị trong mảng
+//  let value = prompt("Enter a number: ");
+//  let numbers = [-3, 5, 1, 3, 2, 10];
+//  for (let i = 0; i < numbers.length; i++) {
+//     if (value == numbers[i]) {
+//             alert("Value " + numbers[i] + "found at " + i);
+//         }
+//     }
+//  Tìm giá trị lớn nhất 
+// let numbers = [-3, 5, 1, 3, 2, 10];
+// let max = numbers[0];
+// let index = 0;
+// for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > max) {
+//         max = numbers[i];
+//         index = i;
+//     }
+// }
+// alert("max: " + max + " at position " + index);
